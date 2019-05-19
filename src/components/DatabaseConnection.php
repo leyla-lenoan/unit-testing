cd51bcb765e862ae0b86d3ff02cc7bf24bd3f3db
Leyla LE NOAN <leyla.lenoan@outlook.fr>
2019-05-19 19:07:27 +0200

    Updated database-connection component

--- src/components/DatabaseConnection.php
+++ src/components/DatabaseConnection.php
@@ -5,20 +5,25 @@ class DatabaseConnection
 	function __construct()
 	{
 		//
-    }
+	}
 
-    public function saveExchange($exchange)
+	public function saveExchange($exchange)
 	{
 		throw new Exception('Not implemented');
 	}
-    
+
 	public function saveProduct($product)
 	{
 		throw new Exception('Not implemented');
-    }
-    
+	}
+
 	public function saveUser($user)
 	{
 		throw new Exception('Not implemented');
-    }
+	}
+
+	public function saveBankAccount($bankAccount)
+	{
+		throw new Exception('Not implemented');
+	}
 }
